858fefa523c1b887c0929763a80cd914e90cec58
chenjiabin <80477286@163.com>
2017-08-24 08:59:38 +0800

    init

---
+++ web-base/src/main/java/com/mouse/web/supports/mvc/bind/annotation/EntityParam.java
@@ -0,0 +1,13 @@
+package com.mouse.web.supports.mvc.bind.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by cwx183898 on 2017/8/17.
+ */
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface EntityParam {
+
+}
