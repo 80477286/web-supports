c2648778a19b5c7be862947324aa47dbbfb63df2
chenjiabin <80477286@163.com>
2017-10-16 17:51:14 +0800

    init

--- web-extjs/src/main/resources/webjars/extjs/6.2.0/extend/navbar/Navbar.js
+++ web-extjs/src/main/resources/webjars/extjs/6.2.0/extend/navbar/Navbar.js
@@ -4,96 +4,160 @@ Ext.define('Extend.navbar.Navbar', {
     baseCls: 'navbar',//基础样式类
     height: 46,
     config: {
-        menuAlign: 'default', itemWidth: null, itemHeight: null
+        menuAlign: 'default',
+        itemWidth: null,
+        itemHeight: null,
+        backgroundColor: '#222',
+
+        itemColor: '#cecece',
+        itemBackgroundColor: '#222',
+
+        itemOverColor: '#FFF',
+        itemOverBackgroundColor: '#535353',
+
+        itemActivedColor: '#FFF',
+        itemActivedBackgroundColor: '#747474'
     },
     childEls: {
         body: {dataRef: 'body'}
     },
     renderTpl: [
+        '<style>',
+        '.navbar-default {background-color: {backgroundColor};}',
+        '.navbar-body-align-left, .navbar-body-align-default {text-align: left;}',
+        '.navbar-body-align-center {text-align: center;}',
+        '.navbar-body-align-right {text-align: right;}',
+        '.navbar-body .ux-menu {position: relative;text-align: center;padding: 0 10px 0 10px;cursor: pointer;font-size: 14px;float: left;}',
+        '.navbar-body .ux-menu a, .navbar-body .ux-menu {color: {itemColor};text-decoration: none;font-size: 18px;}',
+        '.navbar-body .ux-menu-over {background-color: {itemOverBackgroundColor};}',
+        '.navbar-body .ux-menu-over a, .navbar-body .ux-menu-over {color: {itemOverColor};text-decoration: none;font-size: 18px;}',
+        '.navbar-body .ux-menu-actived {background-color: {itemActivedBackgroundColor};}',
+        '.navbar-body .ux-menu-actived a, .navbar-body .ux-menu-actived {color: {itemActivedColor};text-decoration: none;font-size: 18px;}',
+        '</style>',
         '<div id="{id}-body" data-ref="body" class="{baseCls}-body {baseCls}-body-align-{menuAlign}">',
         '</div>'
     ],
     initRenderData: function () {
         var data = this.callParent(arguments);
-        return Ext.apply(data, {menuAlign: this.menuAlign});
+        return Ext.apply(data, {
+            menuAlign: this.menuAlign,
+            backgroundColor: this.backgroundColor,
+            itemColor: this.itemColor,
+            itemBackgroundColor: this.itemBackgroundColor,
+
+            itemOverColor: this.itemOverColor,
+            itemOverBackgroundColor: this.itemOverBackgroundColor,
+
+            itemActivedColor: this.itemActivedColor,
+            itemActivedBackgroundColor: this.itemActivedBackgroundColor
+        });
     },
     onRender: function () {
         this.callParent(arguments);
         this.initItems();
-        console.log(this.body)
     },
     initItems: function () {
         var view = this;
+        view.items = new Ext.util.MixedCollection();
         if (!Ext.isEmpty(this.menus)) {
-            try {
-                for (var i = 0; i < this.menus.length; i++) {
-                    var menu = this.menus[i];
-                    var spec = {tag: 'div', html: menu.name, cls: 'ux-menu'};
-                    var ne = Ext.dom.Helper.append(this.body, spec, true);
-                    var href = menu.href;
-                    var handler = menu.handler;
-                    ne.menu = menu;
-                    ne.applyStyles({height: this.height + 'px', lineHeight: this.height + 'px'});
-                    if (!Ext.isEmpty(view.itemWidth)) {
-                        ne.applyStyles({width: view.itemWidth})
-                    }
-                    if (!Ext.isEmpty(view.itemHeight)) {
-                        ne.applyStyles({width: view.itemHeight})
-                    }
-
-                    if (!Ext.isEmpty(menu.style)) {
-                        ne.applyStyles(menu.style)
-                    }
-                    if (!Ext.isEmpty(menu.icon)) {
-                        ne.applyStyles({
-                            backgroundImage: "url('" + menu.icon + "')",
-                            backgroundRepeat: "no-repeat",
-                            backgroundPosition: 'center center'
-                        });
-                    }
-                    if (Ext.isEmpty(href) && !Ext.isEmpty(handler) && Ext.isFunction(handler)) {
-                        ne.on({
-                            click: handler, scope: ne, args: [ne, menu]
-                        })
-                    }
-                    if (!Ext.isEmpty(href)) {
-                        ne.setHtml()
-                        var a = Ext.dom.Helper.append(ne, {tag: 'a', html: menu.name}, true)
-                        a.set({href: href, target: menu.target});
-                        if (ne.menu.logo == true) {
-                            ne.on({
-                                click: function (_menu) {
-                                    if (_menu.target == '_blank') {
-                                        window.open(_menu.href)
-                                    } else {
-                                        window.location.href = _menu.href
-                                    }
-                                }, scope: ne, args: [ne, menu]
-                            });
-                        }
-                    }
-                    ne.on({
-                        click: function (menu) {
-                            view.fireEvent('itemclick', menu)
-                        }, scope: ne, args: [ne, menu]
-                    });
-                    if (menu.logo != true) {
-                        ne.on({
-                            mouseenter: function () {
-                                this.addCls('ux-menu-over')
-                            },
-                            mouseleave: function () {
-                                this.removeCls('ux-menu-over')
-                            }, scope: ne, args: [menu]
-                        });
-                    }
-                }
-            }
-            catch
-                (e) {
-                console.log({level: 'error', msg: 'failed to create menus!'});
+            for (var i = 0; i < this.menus.length; i++) {
+                var menu = this.menus[i];
+                var ne = view.createMenu(menu);
+                view.items.add(menu.id || menu.name, ne);
             }
         }
+    },
+    createMenu: function (menu) {
+        try {
+            var view = this;
+
+            var spec = {tag: 'div', html: menu.name, cls: 'ux-menu'};
+            var ne = Ext.dom.Helper.append(this.body, spec, true);
+
+            var href = menu.href;
+            var handler = menu.handler;
+            ne.menu = menu;
+            ne.applyStyles({height: this.height + 'px', lineHeight: this.height + 'px'});
+
+            if (!Ext.isEmpty(view.itemWidth)) {
+                ne.applyStyles({width: view.itemWidth})
+            }
+            if (!Ext.isEmpty(view.itemHeight)) {
+                ne.applyStyles({height: view.itemHeight})
+            }
+
+            if (!Ext.isEmpty(menu.style)) {
+                ne.applyStyles(menu.style)
+            }
+            if (!Ext.isEmpty(menu.icon)) {
+                ne.applyStyles({
+                    backgroundImage: "url('" + menu.icon + "')",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: 'center center'
+                });
+            }
+            if (!Ext.isEmpty(handler) && Ext.isFunction(handler)) {
+                ne.on({
+                    click: handler,
+                    scope: ne,
+                    args: [ne, menu]
+                })
+            } else {
+                if (!Ext.isEmpty(href)) {
+                    ne.on({
+                        click: function ($ne, $menu) {
+                            if ($menu.target == '_blank') {
+                                window.open($menu.href);
+                            } else {
+                                window.location.href = $menu.href;
+                            }
+                        },
+                        scope: ne,
+                        args: [ne, menu]
+                    })
+                }
+            }
+
+            ne.active = function () {
+                ne.updateStatus();
+                view.fireEvent('itemclick', ne, menu)
+            }
+            ne.updateStatus = function () {
+                var others = ne.parent().query('.ux-menu', false);
+                if (!Ext.isEmpty(others)) {
+                    Ext.Array.each(others, function (item) {
+                        item.removeCls('ux-menu-actived')
+                    });
+                }
+                ne.addCls('ux-menu-actived')
+            }
+
+            ne.on({
+                click: function ($ne, $menu) {
+                    view.fireEvent('itemclick', $ne, $menu)
+                }, scope: ne, args: [ne, ne.menu]
+            });
+            if (menu.logo != true) {
+                ne.on({
+                    click: function (menu) {
+                        ne.updateStatus();
+                    }, scope: ne, args: [ne, menu]
+                });
+                ne.on({
+                    mouseenter: function () {
+                        this.addCls('ux-menu-over')
+                    },
+                    mouseleave: function () {
+                        this.removeCls('ux-menu-over')
+                    }, scope: ne, args: [menu]
+                });
+            }
+
+            return ne;
+        } catch (e) {
+            console.log({level: 'error', msg: 'failed to create menu!'});
+        }
+        return null;
     },
     initEvents: function () {
         this.callParent(arguments);
